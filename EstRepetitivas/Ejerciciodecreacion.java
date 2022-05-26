92895f85fb45880bb9125020324afc0eddd528ed
kiohunter12 <kioarce12@gmail.com>
2022-05-26 16:21:28 +0000

    123piramide

---
+++ EstRepetitivas/Ejerciciodecreacion.java
@@ -0,0 +1,34 @@
+package EstRepetitivas;
+
+/**
+ * Ejerciciodecreacion
+ */
+
+    import java.util.Scanner;
+
+public class Ejerciciodecreacion {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduzca numero de filas: ");
+        int numFilas = sc.nextInt();
+        sc.close();
+
+        System.out.println();
+        for(int altura = 1; altura<=numFilas; altura++){
+            //Espacios en blanco
+            for(int blancos = 1; blancos<=numFilas-altura; blancos++){
+                System.out.print(" ");
+            }
+
+            //Asteriscos
+            for(int asteriscos=1; asteriscos<=(altura*2)-1; asteriscos++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+    
+
